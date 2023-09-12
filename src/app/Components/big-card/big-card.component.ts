829dc4868ea7a1ef6b15f222e6ecaecfa4a74fbc
Gabriel Toscano <gabrieltoscano1@outlook.com>
2023-09-12 15:22:17 -0300

    Estruturando o componente Big Card

--- src/app/Components/big-card/big-card.component.ts
+++ src/app/Components/big-card/big-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BigCardComponent {
+  imgPath:string = "../../../assets/big-card-img.png"
+  category:string = "Development"
+  createdAt:string = "August 25, 2023"
+  title:string = "How to manage multiple Front-End projects with a monorepo"
+  description:string = "A step by step guide on how to manage multiple Front-End projects with a monorepo."
+  authorName:string = "Rui Saraiva"
+  authorProfession:string = "Front-End Developer"
 }
