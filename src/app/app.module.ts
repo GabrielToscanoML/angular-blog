46f7048e8d14a24b91033dbab3dd46dd113eed4f
Gabriel Toscano <gabrieltoscano1@outlook.com>
2023-09-12 13:07:34 -0300

    Criação dos componentes principais sem os arquivos de tesste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './Components/menu-bar/menu-bar.component';
+import { TitleComponent } from './Components/title/title.component';
+import { BigCardComponent } from './Components/big-card/big-card.component';
+import { SmallCardComponent } from './Components/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    TitleComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
